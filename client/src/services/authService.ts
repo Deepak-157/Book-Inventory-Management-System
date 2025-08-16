c372d1ee14c9cdbb48dd21b3de23a548bb1582a5
Deepak-157 <deepakgoyal26dg@gmail.com>
2025-08-16 06:25:08 +0530

    bugfix:axios not found error resolved

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -7,7 +7,7 @@ import type {
   UserRole,
 } from "../types/auth";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 const api = axios.create({
